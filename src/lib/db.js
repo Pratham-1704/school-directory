71e2c06965705382aa7aa32b0e8d0c8def6c6f2c
Pratham-1704 <prathamkhape2@gmail.com>
2025-09-01 22:47:07 +0530

    db change

--- src/lib/db.js
+++ src/lib/db.js
@@ -2,10 +2,11 @@ import mysql from "mysql2/promise";
 
 export async function connectToDatabase() {
   const connection = await mysql.createConnection({
-    host: "localhost",   // change later when hosting DB
-    user: "root",        // your MySQL username
-    password: "Pass123",// your MySQL password
-    database: "schooldb" // your DB name
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
   });
   return connection;
 }
