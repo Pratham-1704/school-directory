2fecd3ccbcb03a3ad780598027cffb1f765969e3
Pratham-1704 <prathamkhape2@gmail.com>
2025-09-02 11:24:51 +0530

    db problem solved

--- src/app/api/addschool/route.js
+++ src/app/api/addschool/route.js
@@ -13,7 +13,7 @@ export async function POST(req) {
 console.log(name, address, city, state, contact, email_id, image);
     const db = await connectToDatabase();
     await db.execute(
-      "INSERT INTO schools (name, address, city, state, contact, email_id, image) VALUES (?, ?, ?, ?, ?, ?, ?)",
+      "INSERT INTO schools (name, address, city, state, contact, image,email_id) VALUES (?, ?, ?, ?, ?, ?, ?)",
       [name, address, city, state, contact, email_id, image]
     );
 
--- src/lib/db.js
+++ src/lib/db.js
@@ -1,12 +1,23 @@
-import mysql from "mysql2/promise";
+import mysql from 'mysql2/promise';
 
 export async function connectToDatabase() {
-  const connection = await mysql.createConnection({
-    host: process.env.DB_HOST,
-    port: process.env.DB_PORT,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
-  });
-  return connection;
+  try {
+    const connection = await mysql.createConnection({
+      host: process.env.DB_HOST,
+      port: Number(process.env.DB_PORT),
+      user: process.env.DB_USER,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME,
+      ssl: {
+        rejectUnauthorized: false
+       // ca: fs.readFileSync(path.join(__dirname, 'path-to-ca-cert.pem'))
+      }
+    });
+
+    console.log("Connected to DB!");
+    return connection;
+  } catch (error) {
+    console.error("DB connection failed:", error);
+    throw error;
+  }
 }
